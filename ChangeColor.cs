3ba00cb83022260145753b7d8d3696aa7785bd77
Mason Salvas <mason.salvas@gmail.com>
2016-10-06 18:28:06 -0400

    Added the .cs files
    Adding all the .cs files from the project

---
+++ ChangeColor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour {
+
+	Animator animu;
+	public float duration = 1.0F;
+	public Color color0 = Color.red;
+	public Color color1 = Color.green;
+	public Color color2 = Color.yellow;
+	public Light lt;
+
+	void Start() {
+		animu = GetComponent<Animator>();
+		lt = GetComponent<Light>();
+	}
+	void Update() {
+		double t = 1;
+		
+	    if (Input.GetKeyDown (KeyCode.R)) {
+			
+			lt.color = color0;
+			
+		}
+		
+		if (Input.GetKeyDown (KeyCode.G)) {
+			
+			lt.color = color1;
+			
+		}
+		
+		if (Input.GetKeyDown (KeyCode.Y)) {
+			
+			lt.color = color2;
+			
+		}
+		
+	}
+}
---
+++ NewBehaviourScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+///This will move the tanks to the waypoints
+/// </summary>
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed = 2f;
+    public Transform[] waypoints;
+
+
+    private NavMeshAgent nav;
+    private int waypointIndex;
+
+    void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+
+    }
+
+    void Update()
+    {
+        nav.speed = speed;
+        if (nav.remainingDistance < nav.stoppingDistance)
+        {
+            if (waypointIndex == waypoints.Length - 1)
+                waypointIndex = 0;
+            else
+                waypointIndex++;
+        }
+
+        else
+            nav.destination = waypoints[waypointIndex].position;
+    }
+
+
+}
